51b454dfbcfd58c908e4d3bb386589d9313d2572
DimaStephen <dimastephen@proton.me>
2024-10-07 16:47:22 +0400

    proto files completed chatServer

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/fatih/color"
+	"github.com/dimastephen/chatServer/grpc/pkg/chatServerV1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
 )
 
+const addr = "50052"
+
+type server struct {
+	chatServerV1.UnimplementedChatServerServer
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", addr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	chatServerV1.RegisterChatServerServer(s, &server{})
+	log.Println("Started ChatServer")
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Failed to start chatServer", err)
+	}
+
 }
